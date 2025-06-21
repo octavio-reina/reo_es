79f88ee0c3f61f59c12e2ee4b9f2eca41c12d9c9
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-06-21 11:03:22 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,43 @@
-self.addEventListener("install", function (event) {
+const CACHE_NAME = "diccionario-pwa-v1";
+const urlsToCache = [
+  "/diccionario/index.html",
+  "/diccionario/styles.css",
+  "/diccionario/script.js",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
   console.log("Service Worker instalado");
   event.waitUntil(
-    caches.open("v1").then(function (cache) {
-      return cache.addAll(["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== CACHE_NAME) {
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
   );
 });

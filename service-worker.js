898708cbdc93ccd11d3afb02cc33afa69572edb2
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-06-21 01:02:09 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", function (event) {
+  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open("v1").then(function (cache) {
+      return cache.addAll(["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});

82d21cf7e268cf25237a5c8eef5f59afebf1bc83
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-06-26 18:33:12 -0600

    Update datos.js

--- diccionario/js/datos.js
+++ diccionario/js/datos.js
@@ -6,6 +6,7 @@ export const palabras = [];
 export const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTP_ja-WseSt4TpwV3sLoeMjjFcz7NEY8n0CS4mJ12iseR8sjYI-gZ8T_kp1vOd8v2TKVjKPFFT_lW1/pub?gid=0&single=true&output=csv";
 
 export function cargarDatos() {
+
   Papa.parse(CSV_URL, {
     download: true,
     header: true,

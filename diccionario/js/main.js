097bef869e147a551d152d6cc0ac3667cbb765d0
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-06-26 18:08:44 -0600

    Update main.js

--- diccionario/js/main.js
+++ diccionario/js/main.js
@@ -1,5 +1,6 @@
-import { cargarDatos } from './datos.js';
-import { filtrarPalabras, configurarUI } from './ui.js';
+import { cargarDatos } from "./datos.js";
+import { setupUI } from "./ui.js";
+import { filtrarPalabras } from "./filtros.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   if ('serviceWorker' in navigator) {
@@ -7,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(reg => console.log("✅ Service Worker registrado:", reg.scope))
       .catch(err => console.error("❌ Error al registrar Service Worker:", err));
   }
-  configurarUI();
-  cargarDatos().then(filtrarPalabras);
+
+  setupUI();
+  cargarDatos();
 });

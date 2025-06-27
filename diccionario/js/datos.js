cbe7b85dc0e10350d792b7a07e364f278c4b4799
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-06-26 18:09:16 -0600

    Update datos.js

--- diccionario/js/datos.js
+++ diccionario/js/datos.js
@@ -1,21 +1,22 @@
+import { ordenarAlfabeticamente } from "./util.js";
+import { generarFiltros } from "./filtros.js";
+import { mostrarPalabras } from "./tarjeta.js";
+
+export const palabras = [];
 export const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTP_ja-WseSt4TpwV3sLoeMjjFcz7NEY8n0CS4mJ12iseR8sjYI-gZ8T_kp1vOd8v2TKVjKPFFT_lW1/pub?gid=0&single=true&output=csv";
 
-export let palabras = [];
-
-export async function cargarDatos() {
-  return new Promise((resolve, reject) => {
-    Papa.parse(CSV_URL, {
-      download: true,
-      header: true,
-      complete: function(results) {
-        palabras = results.data;
-        palabras.sort((a, b) => a["Reo Tahiti"].localeCompare(b["Reo Tahiti"], 'es', { sensitivity: 'base' }));
-        resolve(palabras);
-      },
-      error: function(err) {
-        document.getElementById("resultados").innerHTML = `<p>Error al cargar datos: ${err.message}</p>`;
-        reject(err);
-      }
-    });
+export function cargarDatos() {
+  Papa.parse(CSV_URL, {
+    download: true,
+    header: true,
+    complete: function(results) {
+      palabras.splice(0, palabras.length, ...results.data);
+      ordenarAlfabeticamente(palabras);
+      generarFiltros(palabras);
+      mostrarPalabras(palabras);
+    },
+    error: function(err) {
+      document.getElementById("resultados").innerHTML = `<p>Error al cargar datos: ${err.message}</p>`;
+    }
   });
 }

fb51cf1ebd18fcbd8799b4c239ef1bfd5b6addd7
octavio-reina <89221707+octavio-reina@users.noreply.github.com>
2025-07-14 12:44:51 -0600

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,5 +1,5 @@
 const CLAVE_SECRETA = "maitaimaitai";
-const URL_BASE = "https://script.google.com/macros/s/AKfycby5lPoCptk-EEG69coTMV-iWk-Tj_FbHTR5bMb3zm-oGEE6pqifhvvMpo5t5g14hgpb/exec";
+const URL_BASE = "https://script.google.com/macros/s/AKfycbxKWwfnzUyzYxteXi4Wo0sDNydNh2UXZR622kMc7SYy-tOSETK3uvz_-DxfjCilU0WK/exec";
 
 let editandoID = null;
 let palabrasCache = [];
